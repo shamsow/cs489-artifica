cc09b3b382b15b0961f0f514f213b33f7c698a4d
shamsow <sadatshams@gmail.com>
2023-11-30 14:00:40 +0900

    Fix image format bugs, add error logging

--- server/api/views.py
+++ server/api/views.py
@@ -15,16 +15,22 @@ model = BeitForImageClassification.from_pretrained('TimKond/diffusion-detection'
 def check_image(request):
     try:
         image_url = request.data.get('url')
-        image = Image.open(requests.get(image_url, stream=True).raw)
+        image_response = requests.get(image_url, stream=True)
+        if image_response.status_code != 200:
+            return Response({"message": f"Request to {image_url} failed with status code: {image_response.status_code}"}, status=int(image_response.status_code))
         
+        image = Image.open(image_response.raw).convert("RGB")
+
         inputs = processor(images=image, return_tensors="pt")
         outputs = model(**inputs)
         logits = outputs.logits
         predicted_class_idx = logits.argmax(-1).item()
         
         return Response({"label": model.config.id2label[predicted_class_idx]}, status=status.HTTP_200_OK)
-    except:
+    except Exception as e:
+        print("error: ", e)
         return Response({"message": "Something went wrong :("}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def report(request):
